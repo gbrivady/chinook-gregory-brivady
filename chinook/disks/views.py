a55e3c313ffeb6713a0d0b9d426fd17cbbbc10d0
Gregory Brivady <gregory.brivady@student.isae-supaero.fr>
2022-04-15 21:40:11 +0200

    create view for album search results

--- chinook/disks/views.py
+++ chinook/disks/views.py
@@ -1,3 +1,5 @@
+from re import template
+from unicodedata import name
 from django.shortcuts import render
 from django.views import generic
 
@@ -16,4 +18,14 @@ class DetailView(generic.DetailView):
     template_name = "disks/detail.html"
     def get_queryset(self):
         return Album.objects
+
+class SearchResultsView(generic.ListView):
+    model = Album
+    template_name = 'disks/search_results.html'
+    context_object_name = 'found_album_list'
+    
+    def get_queryset(self):
+        user_search = self.request.GET.get("search")
+        return Album.objects.filter(title__icontains=user_search).order_by('title')
+    
     
