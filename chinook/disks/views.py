551f244068012b6cce15fa7c9bf80523902e3bb0
Gregory Brivady <gregory.brivady@student.isae-supaero.fr>
2022-04-15 09:38:49 +0200

    add index view
    template to display the list of all albums, by alphabetical order

--- chinook/disks/views.py
+++ chinook/disks/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Artist, Album, Track
+
+class IndexView(generic.ListView):
+    template_name = 'disks/index.html'
+    context_object_name = 'all_disks_list'
+
+    def get_queryset(self):
+        # Return all the albums, ordered by title in alphabetical order
+        return Album.objects.order_by('title')
