b5a3b5ca434d40bbf7b400cab554db9e79214cb4
Gregory Brivady <gregory.brivady@student.isae-supaero.fr>
2022-04-15 21:40:54 +0200

    route search result page

--- chinook/disks/urls.py
+++ chinook/disks/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "disks"
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail')
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('search/', views.SearchResultsView.as_view(), name='search_results')
 ]
