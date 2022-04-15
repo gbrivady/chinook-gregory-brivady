08d7eae006abfe6fc69c45cb2e2fa6b730013efc
Gregory Brivady <gregory.brivady@student.isae-supaero.fr>
2022-04-15 09:36:28 +0200

    create models
    Create the Artist, Album and Track models to work with the chinook.db database

--- chinook/disks/models.py
+++ chinook/disks/models.py
@@ -1,3 +1,37 @@
+from operator import mod
 from django.db import models
 
-# Create your models here.
+class Artist(models.Model):
+    """
+    An artist that may have some albums
+    """
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+
+class Album(models.Model):
+    """
+    An album by one artist with several tracks
+    """
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    
+    title = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.title
+    
+class Track(models.Model):
+    """
+    A single track in an album
+    """
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    
+    name = models.CharField(max_length=200)
+    composer = models.CharField(max_length=200)
+    milliseconds = models.IntegerField('Duration (ms)')
+    bytes = models.IntegerField('Size (bytes)')
+    unitPrice = models.DecimalField('Unit Price (EUR)', max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
